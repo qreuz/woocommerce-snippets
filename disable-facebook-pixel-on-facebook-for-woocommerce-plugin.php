c61518721aba868a4d7bb45c9064ea21242afade
Matthias Reinholz <mail@matthiasreinholz.com>
2019-03-13 15:40:44 +0100

    update to facebook pixel remove snippet

--- disable-facebook-pixel-on-facebook-for-woocommerce-plugin.php
+++ disable-facebook-pixel-on-facebook-for-woocommerce-plugin.php
@@ -1,24 +1,22 @@
 <?php
 
 /**
-
- ██████╗ ██████╗ ███████╗██╗   ██╗███████╗
-██╔═══██╗██╔══██╗██╔════╝██║   ██║╚══███╔╝
-██║   ██║██████╔╝█████╗  ██║   ██║  ███╔╝ 
-██║▄▄ ██║██╔══██╗██╔══╝  ██║   ██║ ███╔╝  
-╚██████╔╝██║  ██║███████╗╚██████╔╝███████╗
- ╚══▀▀═╝ ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝
-                                          
-
-QREUZ SNIPPET FOR WOOCOMMERCE
-
-This file is part of a collection of snippets to be used on your WooCommerce store.
-!! Do not put this file in your Wordpress installation !!
-
-Copy/paste the code below and add it to the functions.php of your child theme.
-Don't know what a child theme is? Read this post: https://qreuz.com/how-to-use-a-child-theme-on-wordpress-and-woocommerce/
-
-COPY AND PASTE EVERYTHING BELOW THIS LINE TO YOUR FUNCTIONS.PHP **/
+ *
+ * ██████╗ ██████╗ ███████╗██╗   ██╗███████╗
+ *██╔═══██╗██╔══██╗██╔════╝██║   ██║╚══███╔╝
+ *██║   ██║██████╔╝█████╗  ██║   ██║  ███╔╝ 
+ *██║▄▄ ██║██╔══██╗██╔══╝  ██║   ██║ ███╔╝  
+ *╚██████╔╝██║  ██║███████╗╚██████╔╝███████╗
+ * ╚══▀▀═╝ ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝
+ *                                          
+ * QREUZ SNIPPET FOR WOOCOMMERCE
+ * This file is part of a collection of snippets to be used on your WooCommerce store.
+ * !! Do not put this file in your Wordpress installation !!
+ *
+ * Copy/paste the code below and add it to the functions.php of your child theme.
+ * Don't know what a child theme is? Read this post: https://qreuz.com/how-to-use-a-child-theme-on-wordpress-and-woocommerce/
+ *
+ * COPY AND PASTE EVERYTHING BELOW THIS LINE TO YOUR FUNCTIONS.PHP **/
 
 
 /**
