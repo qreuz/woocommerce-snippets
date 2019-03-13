9d291bbe19403d1581ddc7837d3e830c5393448c
Matthias Reinholz <mail@matthiasreinholz.com>
2019-03-13 15:38:15 +0100

    snippet upload Disable Facebook Pixel on Facebook for WooCommerce Plugin

---
+++ disable-facebook-pixel-on-facebook-for-woocommerce-plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+
+ ██████╗ ██████╗ ███████╗██╗   ██╗███████╗
+██╔═══██╗██╔══██╗██╔════╝██║   ██║╚══███╔╝
+██║   ██║██████╔╝█████╗  ██║   ██║  ███╔╝ 
+██║▄▄ ██║██╔══██╗██╔══╝  ██║   ██║ ███╔╝  
+╚██████╔╝██║  ██║███████╗╚██████╔╝███████╗
+ ╚══▀▀═╝ ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝
+                                          
+
+QREUZ SNIPPET FOR WOOCOMMERCE
+
+This file is part of a collection of snippets to be used on your WooCommerce store.
+!! Do not put this file in your Wordpress installation !!
+
+Copy/paste the code below and add it to the functions.php of your child theme.
+Don't know what a child theme is? Read this post: https://qreuz.com/how-to-use-a-child-theme-on-wordpress-and-woocommerce/
+
+COPY AND PASTE EVERYTHING BELOW THIS LINE TO YOUR FUNCTIONS.PHP **/
+
+
+/**
+ * QREUZ SNIPPET FOR WOOCOMMERCE
+ * @TITLE: Disable Facebook Pixel on Facebook for WooCommerce Plugin
+ * @FOR_PLUGIN: Facebook for WooCommerce, https://woocommerce.com/products/facebook/
+ * @DESCRIPTION: disables the Facebook Pixel on the Facebook for WooCommerce plugin
+ * @SOURCE: https://qreuz.com
+ * @AUTHOR: Qreuz GmbH
+ */		
+add_filter('facebook_for_woocommerce_integration_pixel_enabled', '__return_false', 20);
