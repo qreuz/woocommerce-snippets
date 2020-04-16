648c40f905e3cb8a9e07832f045c5f865884f119
Matthias Reinholz <mail@matthiasreinholz.com>
2020-04-16 08:30:57 +0200

    upload of disable woocommerce store notice snippet

---
+++ disable-woocommerce-connect-your-store-admin-notice.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ *  ██████╗ ██████╗ ███████╗██╗   ██╗███████╗
+ * ██╔═══██╗██╔══██╗██╔════╝██║   ██║╚══███╔╝
+ * ██║   ██║██████╔╝█████╗  ██║   ██║  ███╔╝
+ * ██║▄▄ ██║██╔══██╗██╔══╝  ██║   ██║ ███╔╝
+ * ╚██████╔╝██║  ██║███████╗╚██████╔╝███████╗
+ *  ╚══▀▀═╝ ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝
+ *
+ * QREUZ SNIPPET FOR WOOCOMMERCE
+ * This file is part of a collection of snippets to be used on your WooCommerce store.
+ * !! Do not put this file in your WordPress installation !!
+ *
+ * Copy/paste the code below and add it to the functions.php of your child theme.
+ * Don't know what a child theme is? Read this post: https://qreuz.com/
+ *
+ * COPY AND PASTE EVERYTHING BELOW THIS LINE TO YOUR FUNCTIONS.PHP */
+
+/**
+ * QREUZ SNIPPET FOR WOOCOMMERCE / WORDPRESS
+ *
+ * @TITLE       Disable the "Connect your store to WooCommerce.com to receive extensions updates and support" admin notice
+ * @VERSION     1.0
+ * @DESCRIPTION Removes the admin notice "Connect your store to WooCommerce.com to receive extensions updates and support" from your WordPress backend
+ * @LINK        https://qreuz.com/snippets/
+ * @AUTHOR      Qreuz GmbH <hello@qreuz.com>
+ * @LICENSE     GNU GPL v3 https://www.gnu.org/licenses/gpl-3.0
+ */
+
+/****/
+add_filter( 'woocommerce_helper_suppress_admin_notices', '__return_true' );
